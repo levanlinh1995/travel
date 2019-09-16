db5f1ae2eeb02e0e19930291d7b16d9cc8638ff8
le van linh <levanlinh1995@gmail.com>
2019-09-16 12:26:16 +0700

    add vuetify plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/index'
+import store from './store/index'
+import vuetify from './plugins/vuetify'
+import '@babel/polyfill'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
