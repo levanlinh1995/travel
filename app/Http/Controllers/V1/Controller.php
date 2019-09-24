955d6ac87b7b196d7f6bd595362f812d339cb09a
le van linh <levanlinh1995@gmail.com>
2019-09-24 12:17:25 +0700

    install and set up fractal transformer

--- app/Http/Controllers/V1/Controller.php
+++ app/Http/Controllers/V1/Controller.php
@@ -3,8 +3,48 @@
 namespace App\Http\Controllers\V1;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use League\Fractal\Manager;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
+use League\Fractal\Serializer\JsonApiSerializer;
 
 class Controller extends BaseController
 {
-    //
+    private function getFractalManager()
+    {
+        $request = app(Request::class);
+        $manager = new Manager();
+        $manager->setSerializer(new JsonApiSerializer());
+
+        if (!empty($request->query('include'))) {
+            $manager->parseIncludes($request->query('include'));
+        }
+
+        return $manager;
+    }
+
+    public function item($data, $transformer)
+    {
+        $manager = $this->getFractalManager();
+        $resource = new Item($data, $transformer);
+        return $manager->createData($resource)->toArray();
+
+    }
+
+    public function collection($data, $transformer)
+    {
+        $manager = $this->getFractalManager();
+        $resource = new Collection($data, $transformer);
+        return $manager->createData($resource)->toArray();
+    }
+
+    public function paginate($data, $transformer)
+    {
+        $manager = $this->getFractalManager();
+        $resource = new Collection($data, $transformer, $transformer->type);
+        $resource->setPaginator(new IlluminatePaginatorAdapter($data));
+        return $manager->createData($resource)->toArray();
+    }
 }
---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App\Transformers;
+
+use App\Transformers\Transformer;
+
+class UserTransformer extends Transformer
+{
+    public function transform($user)
+    {
+        
+    }
+}
